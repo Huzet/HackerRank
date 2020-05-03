05e96e24a2815f31e86979e03ae8cfe4cea6a279
Tomas <tomas.huzevka@outlook.com>
2020-05-03 19:13:48 -0400

    SimpleArraySum

--- CompareTriplets.py
+++ CompareTriplets.py
@@ -20,7 +20,7 @@ def compareTriplets(a, b):
         else:
             bob=bob+1
     return(alice, bob)
-    
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
@@ -34,6 +34,3 @@ if __name__ == '__main__':
     fptr.write('\n')
 
     fptr.close()
-
-
-    
