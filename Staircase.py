b62887fa32954946f7778ad31ac875296cfb7804
Tomas <tomas.huzevka@outlook.com>
2020-06-12 00:29:53 -0400

    quick change

--- Staircase.py
+++ Staircase.py
@@ -9,16 +9,17 @@ import re
 import sys
 
 # Complete the staircase function below.
+def str_append(s, n):
+    output = ''
+    i = 0
+    while i < n:
+        output = output + s
+        i = i + 1
+    return output
 
 def staircase(n):
     for x in range(1,n+1):
-        stairs = ''
-        i = 0
-        while i < x:
-            stairs += '#'
-            i = i + 1
-        print(stairs)
-        
+        print(str_append('#', x).rjust(n))
 
 if __name__ == '__main__':
     n = int(input())
