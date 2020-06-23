befa1a21bd406b8dabf8b93988ae31771b913cd1
Tomas <tomas.huzevka@outlook.com>
2020-06-23 00:09:50 -0400

    was not passing some cases

--- Capitalize.py
+++ Capitalize.py
@@ -6,23 +6,23 @@ import random
 import re
 import sys
 
-s = 'adam bom g3 3g'
+# https://www.hackerrank.com/challenges/capitalize/problem
 
 def solve(s):
-    title = s.title()
-    print(title)
-    return title
-    
+    mylist = []
+    title = s.split(" ")
+    for x in title:
+        mylist.append(x.capitalize()) 
+    return " ".join(mylist)
 
-solve(s)
 
-# if __name__ == '__main__':
-#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-#     s = input()
+    s = input()
 
-#     result = solve(s)
+    result = solve(s)
 
-#     fptr.write(result + '\n')
+    fptr.write(result + '\n')
 
-#     fptr.close()
+    fptr.close()
