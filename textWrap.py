a848f4e855173e643b746270a7eeaff4b7929aff
Tomas <tomas.huzevka@outlook.com>
2020-06-24 00:34:01 -0400

    textwrap complete

--- textWrap.py
+++ textWrap.py
@@ -1,14 +1,20 @@
 import textwrap
 
+# https://www.hackerrank.com/challenges/text-wrap/problem
+
+
 def wrap(string, max_width):
-    newstring = []
-    newstring = string.split(" ",max_width)
-    print( newstring )
+    mylist = []
+    mystring = ""
+    for x in range(0, len(string), max_width):
+        variable = string[x:x+max_width]
+        mylist.append(variable)
+
+    mystring = "\n".join(mylist)
+    return mystring       
 
 
-wrap('ABCDEFGHIJKLIMNOQRSTUVWXYZ', 4)
-
-# if __name__ == '__main__':
-#     string, max_width = raw_input(), int(raw_input())
-#     result = wrap(string, max_width)
-#     print(result)
+if __name__ == '__main__':
+    string, max_width = raw_input(), int(raw_input())
+    result = wrap(string, max_width)
+    print(result)
