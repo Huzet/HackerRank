2b3b41cdc4192e92b61634af0ab55dd59642d398
Tomas <tomas.huzevka@outlook.com>
2020-06-27 02:27:01 -0400

    finished time conversion

--- time_conversion.py
+++ time_conversion.py
@@ -1,16 +1,49 @@
-#!/bin/python3
-# 
-# 
+#!/bin/python
 
-time = "5:05:45AM"
-newtime = time.split(":")
+from __future__ import print_function
 
-if newtime[2][2] == "P":
-    newtime[0] = int(newtime[0]) + 12
-    print(newtime)
-elif newtime[2][2] == "A" and int(newtime[0]) == 12:
-    newtime[0] = str('00')
-    print(newtime)
-elif newtime[2][2] == "A":
-    newtime[0] = "0" + newtime[0]
-    print(newtime)
+import os
+import sys
+
+#
+# Complete the timeConversion function below.
+#
+def timeConversion(s):
+    newtime = s.split(":")
+    if newtime[2][2] == "P" and int(newtime[0]) == 12:
+        newtime[0] = str('12')
+        newtime[2] = newtime[2].replace('PM', '')
+        newtime[0] = str(newtime[0])
+        newtime = str(':'.join(newtime))
+        return newtime
+
+    elif newtime[2][2] == "P":
+        newtime[0] = int(newtime[0]) + 12
+        newtime[2] = newtime[2].replace('PM', '')
+        newtime[0] = str(newtime[0])
+        newtime = str(':'.join(newtime))
+        return newtime
+
+    elif newtime[2][2] == "A" and int(newtime[0]) == 12:
+        newtime[0] = str('00')
+        newtime[2] = newtime[2].replace('AM', '')
+        newtime[0] = str(newtime[0])
+        newtime = str(':'.join(newtime))
+        return newtime
+
+    elif newtime[2][2] == "A":
+        newtime[2] = newtime[2].replace('AM', '')
+        newtime[0] = str(newtime[0])
+        newtime = str(':'.join(newtime))
+        return newtime
+
+if __name__ == '__main__':
+    # f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    # f.write(result + '\n')
+
+    # f.close()
