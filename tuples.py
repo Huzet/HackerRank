7a06ee142cfd745df3f48572b8c2c7bff7981ada
Tomas <tomas.huzevka@outlook.com>
2020-06-30 21:33:54 -0400

    hashing tuples

---
+++ tuples.py
@@ -0,0 +1,14 @@
+import hashlib
+
+# https://www.hackerrank.com/challenges/python-tuples/problem
+
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    mylist = tuple(integer_list)
+    answer = 0
+    for x in range(0,n):
+        answer = answer + mylist[x]
+    answers= hash(mylist)
+    print(answers)
