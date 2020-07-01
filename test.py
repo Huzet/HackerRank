4bb63ec16239dea05ac37964ff03e808c001da7e
Tomas <tomas.huzevka@outlook.com>
2020-06-30 21:32:40 -0400

    hashing tuples

--- test.py
+++ test.py
@@ -1,17 +1,13 @@
-def myround(x, base=5):
-    return base * round(x/base)
-
-def newround(x, base=5):
-    while x % 5 != 0:
-        x = x + 1
-    return x
-
-# 67 73
-# 2 * 3 = 6 multiply
-# 6 / 3 = 2 devide
-# 12 % 7 = 5 Modulous
+import hashlib
 
 
-test = map(newround, (73, 67, 38, 33))
-print(list(test))
-# print(round(7))
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    mylist = tuple(integer_list)
+    answer = 0
+    for x in range(0,n):
+        answer = answer + mylist[x]
+    answers= hash(mylist)
+    print(answers)
